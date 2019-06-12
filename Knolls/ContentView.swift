def6ef23b77fecb46709911e4bfd7b7716f0b950
Ricky Padilla <rickycpadilla@gmail.com>
2019-06-12 07:04:03 -0600

    Initial Commit

---
+++ Knolls/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Knolls
+//
+//  Created by Ricky Padilla on 6/12/19.
+//  Copyright © 2019 Leef Studios, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ KnollsTests/KnollsTests.swift
@@ -0,0 +1,34 @@
+//
+//  KnollsTests.swift
+//  KnollsTests
+//
+//  Created by Ricky Padilla on 6/12/19.
+//  Copyright © 2019 Leef Studios, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Knolls
+
+class KnollsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
